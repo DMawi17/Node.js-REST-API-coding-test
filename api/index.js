af8bda0985d0a605ad8de0d3e93312e5fde87302
DMawi17 <75326045+DMawi17@users.noreply.github.com>
2022-06-25 08:08:48 +0200

    INR-4 Write the configuration and test the endpoint 'artist.search'.

---
+++ api/index.js
@@ -0,0 +1,31 @@
+const axios = require("axios");
+const { apiKey } = require("../config");
+
+const API_ROOT_URL = "http://ws.audioscrobbler.com/2.0";
+const ARTIST_SEARCH = "/?method=artist.search";
+
+const client = axios.create({
+    baseURL: API_ROOT_URL,
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+const params = new URLSearchParams();
+const request = {
+    params,
+};
+
+const fetchArtistByName = (str, params = request) => {
+    request.params.append("artist", str);
+    request.params.append("api_key", apiKey);
+    request.params.append("format", "json");
+
+    return client.get(ARTIST_SEARCH, params);
+};
+
+fetchArtistByName("summer walker").then((a) =>
+    console.log(a.data.results.artistmatches.artist)
+);
+
+module.exports = fetchArtistByName;
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const { port } = require("./config");
 
 const app = express();
 
-app.listen(3000, (err) => {
+app.get("/", (req, res) => {
+    res.send("It works");
+});
+
+app.listen(port, (err) => {
     if (err) {
         console.log(err);
     }
-    console.info("Server started on port %s.", 3000);
+    console.info("Server started on port %s.", port);
 });
